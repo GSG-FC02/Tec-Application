c49a282dd2a16d405f8cb6a171e2a940b116be0c
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-04-12 16:58:18 +0300

    add element selectors and simple edit of html and css

---
+++ search-page/search.js
@@ -0,0 +1,10 @@
+/* elements selectors */ 
+
+const input = document.getElementById("inputSearch");
+const searchAction = document.getElementById("searchIcon");
+const wordAudio = document.getElementById("eleAudio");
+const actionLoop = document.getElementById("action-loop");
+const selectPhoto = document.getElementById("Photo-fetched");
+
+/************************************************************************************************ */
+
