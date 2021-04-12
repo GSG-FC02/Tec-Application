e57f39c6c45693e66471b2ec2697c02e5e62adf2
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-04-12 18:28:19 +0300

    clean code and create function for select element

--- search-page/search.js
+++ search-page/search.js
@@ -1,25 +1,24 @@
-/* elements selectors */ 
+/* function for elements selectors */
 
-const input = document.getElementById("inputSearch");
-const searchAction = document.getElementById("searchIcon");
-const wordAudio = document.getElementById("eleAudio");
-const actionLoop = document.getElementById("action-loop");
-const selectPhoto = document.getElementById("Photo-fetched");
+let selector = (select) => {
+    return document.querySelector(select)
+}
+
+// Exmple : selector("#input-search") <=> document.getElementById("input-search");
 
 /************************************************************************************************ */
 
 // function working to fetch audio of the entered word from API
 
-searchAction.addEventListener("click", enteredWord )
+selector("#searchIcon").addEventListener("click", enteredWord )
 
 function enteredWord() {
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${input.value}` , {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${selector("#input-search").value}` , {
     method: 'GET',
 })
     .then(response => { return response.json() })
     .then(data => {
-        console.log (data[0].phonetics[0].audio)
-        wordAudio.setAttribute("src" , data[0].phonetics[0].audio);
+        selector("#element-Audio").setAttribute("src" , data[0].phonetics[0].audio);
     })
     .catch(error => { console.log('Something went wrong', error);
     });
@@ -29,16 +28,15 @@ function enteredWord() {
 
 // function working to fetch the image of based on the entered word from API
 
-searchAction.addEventListener("click", fetchPhoto )
+selector("#searchIcon").addEventListener("click", fetchPhoto )
 
 function fetchPhoto() {
-    fetch(`https://api.unsplash.com/search/photos?query=${input.value}&client_id=vc40VGPe_rbHs0hMlQ4Z9UzWhPWW94MF_mmqLH7219E` , {
+    fetch(`https://api.unsplash.com/search/photos?query=${selector("#input-search").value}&client_id=vc40VGPe_rbHs0hMlQ4Z9UzWhPWW94MF_mmqLH7219E` , {
     method: 'GET',
 })
     .then(response => { return response.json() })
     .then(data => {
-        console.log(data.results[0].urls.small)
-        selectPhoto.setAttribute("src" , data.results[0].urls.small);
+        selector("#Photo-fetched").setAttribute("src" , data.results[0].urls.small);
     })
     .catch(error => { console.log('Something went wrong', error);
     });
@@ -48,10 +46,10 @@ function fetchPhoto() {
 
 // function working a repeat of pronunciation the word
 
-actionLoop.addEventListener("click", loopAudio )
+selector("#action-loop").addEventListener("click", loopAudio )
 
 function loopAudio() {
-    wordAudio.play();
+    selector("#element-Audio").play();
 }
 
 /************************************************************************************************ */
