0db7338f746c1a3541a2efa7848eb2d86134e9f9
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-04-12 17:12:36 +0300

    create function for fetch audio from api

--- search-page/search.js
+++ search-page/search.js
@@ -8,3 +8,21 @@ const selectPhoto = document.getElementById("Photo-fetched");
 
 /************************************************************************************************ */
 
+// function working to fetch audio of the entered word from API
+
+searchAction.addEventListener("click", enteredWord )
+
+function enteredWord() {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${input.value}` , {
+    method: 'GET',
+})
+    .then(response => { return response.json() })
+    .then(data => {
+        console.log (data[0].phonetics[0].audio)
+        wordAudio.setAttribute("src" , data[0].phonetics[0].audio);
+    })
+    .catch(error => { console.log('Something went wrong', error);
+    });
+}
+
+/************************************************************************************************ */
