3c3f2d667dbfa25dbece66839c73717f75b41f3d
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-04-14 19:01:53 +0300

    Onload: getting data from local storage and create buttons with the data appended in the buttons Relates #20

--- history-page/history.js
+++ history-page/history.js
@@ -12,24 +12,31 @@ let selector = (select) => {
   // HTML Elements
 const wordDiv = selector('.words-container');
 
-// slapping data from local storage when onloud
+/* slapping data from local storage when onloud */
 window.onload = function () {
     let oldData = JSON.parse(localStorage.getItem("data"));
-    console.log(oldData)
+    /* If there is data in local storage, slap it to browser */ 
     if (oldData != null) {
-        for (let i=0; i< oldData.length; i++) {
-            const wordButton = creatElement('button')
-            wordButton.textcontent = oldData[i].name;
+        for (let i=0; i< oldData.length; i++) { // loop on the array of objects in local storage
+            /* create the button */
+            const wordButton = creatElement('button') 
+            wordButton.textContent = oldData[i].name;
             appendElement(wordDiv, wordButton )
+            /* create remove icon */
             let removeIcon = creatElement('img')
             removeIcon.setAttribute('src', '../images/removeicon .png')
-            removeIcon.classlist.add('remove-icon')
+            removeIcon.classList.add('remove-icon')
             appendElement(wordButton, removeIcon)
+            /* append audio into the button */
             let audio = creatElement('audio')
             audio.setAttribute('src', oldData[i].audioSet)
+            appendElement(wordButton, audio)
+          
   
         }
       }
    
    
   };
+
+
