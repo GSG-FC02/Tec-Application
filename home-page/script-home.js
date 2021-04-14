e7f5d70a3cb0530d7311c4708adc6af1cf7c8309
Noor Abunamus <noorin.sa.99@gmail.com>
2021-04-13 20:12:45 -0700

    changing some properities at the style of speaker icon Relates #17

--- home-page/script-home.js
+++ home-page/script-home.js
@@ -4,5 +4,5 @@ speaker.addEventListener('click' , displayAudio);
 
 
 function displayAudio() {
-    Audio.setAttribute("src" ,"http://api.voicerss.org/?key=b122fe77a30a4863ab2fdd805d455ccf&hl=en-us&c=MP3&f=16khz_16bit_stereo&v=Linda&src=Hi,%20Iam%20tec.............%20Lets%20have%20some%20good%20time%20learning%20english%20together.");
+    Audio.setAttribute("src" ,"http://api.voicerss.org/?key=b122fe77a30a4863ab2fdd805d455ccf&hl=en-us&c=MP3&f=16khz_16bit_stereo&v=Mary&src=Hi,%20Iam%20tec.............%20Lets%20have%20some%20good%20time%20learning%20english%20together.");
 }
