4f50578e0bceff087369bbd147ebe576325d270f
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-04-14 18:29:53 +0300

    create button and auidio and append them to the words container with giving a style to the audio Relattes #20

--- history-page/history.js
+++ history-page/history.js
@@ -16,6 +16,20 @@ const wordDiv = selector('.words-container');
 window.onload = function () {
     let oldData = JSON.parse(localStorage.getItem("data"));
     console.log(oldData)
+    if (oldData != null) {
+        for (let i=0; i< oldData.length; i++) {
+            const wordButton = creatElement('button')
+            wordButton.textcontent = oldData[i].name;
+            appendElement(wordDiv, wordButton )
+            let removeIcon = creatElement('img')
+            removeIcon.setAttribute('src', '../images/removeicon .png')
+            removeIcon.classlist.add('remove-icon')
+            appendElement(wordButton, removeIcon)
+            let audio = creatElement('audio')
+            audio.setAttribute('src', oldData[i].audioSet)
+  
+        }
+      }
    
    
   };
