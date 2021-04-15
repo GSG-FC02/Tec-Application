51923189f4a56708ed516c56b314e722dc58f21f
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-04-15 14:37:56 +0300

    add line of code for low volume music

--- home-page/script-home.js
+++ home-page/script-home.js
@@ -3,9 +3,14 @@ const Audio = document.getElementById('ele-Aduio');
 const backgroundMusic = document.getElementById('background-music');
 speaker.addEventListener('click' , displayAudio);
 
+backgroundMusic.volume = 0.3;
 document.onload(backgroundMusic.play());
 
 
 function displayAudio() {
     Audio.setAttribute("src" ,"http://api.voicerss.org/?key=b122fe77a30a4863ab2fdd805d455ccf&hl=en-us&c=MP3&f=16khz_16bit_stereo&v=Mary&src=Hi,%20Iam%20tec.............%20Lets%20have%20some%20good%20time%20learning%20english%20together.");
+    backgroundMusic.pause();
+    setTimeout(() => {
+        backgroundMusic.play()
+    }, 6000);
 }
